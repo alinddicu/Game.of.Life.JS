6285c4c1ac494c1be6738de8fb75d8dbc439cfe6
alinddicu <alinddicu@yahoo.com>
2014-11-26 22:46:11 +0100

    JS sucks

--- Game.of.Life.JS/scripts/Cell.js
+++ Game.of.Life.JS/scripts/Cell.js
@@ -1,7 +1,42 @@
 var GameOfLife = GameOfLife || {};
 
-GameOfLife.Cell = function(cellState) {
-  self = this;
+GameOfLife.Cell = function(x, y, cellState) {
+    self = this;
 
-  self.CurrentState = CellState.Alive;
+    self.X = x;
+    self.Y = y;
+    self.CurrentState = cellState;
+    self.NextState = null;
+    self.Neighbours = [];
+
+    self.AddNeighbours = function (cells) {
+      if(self.Neighbours.length > 8){
+        throw "The cell can't have more than 8 neighbours";
+      }
+
+      _.each(cells, function(cell){
+        /**/
+        self.Neighbours = _.reject(self.Neighbours, function(neighbour) {
+          return neighbour.X == cell.X && neighbour.Y == cell.Y;
+        });
+        /**/
+
+        self.Neighbours.push(cell);
+      });
+    };
+
+    self.Mutate = function() {
+        self.NextState = self.CurrentState;
+        var aliveNeighboursCount = _.filter(self.Neighbours, function(neighbour){ return neighbour.CurrentState == CellState.Alive;}).length;
+        console.log(aliveNeighboursCount);
+        if (aliveNeighboursCount < 2 || aliveNeighboursCount > 3) {
+            console.log("CellState.Dead");
+            self.NextState = CellState.Dead;
+            console.log(self.NextState);
+        } else if (self.CurrentState == CellState.Dead && aliveNeighboursCount == 3) {
+            console.log("CellState.Alive");
+            self.NextState = CellState.Alive;
+        }
+        console.log(self.NextState);
+    };
 };
--- Game.of.Life.JS/test/spec/CellSpec.js
+++ Game.of.Life.JS/test/spec/CellSpec.js
@@ -1,9 +1,19 @@
 describe("Cell", function() {
-  
-  it("GivenNewCellWhenGetStateThenTheCurrenStateIsAlive", function() {
-    var cell = new GameOfLife.Cell();
-	
-    expect(cell.CurrentState).toEqual(CellState.Alive);
-	expect(cell.CurrentState).not.toEqual(CellState.Dead);
+
+  it("Given New Cell When GetState Then The CurrenState Is Alive", function() {
+      var cell = new GameOfLife.Cell(1, 1);
+
+      expect(cell.CurrentState).toEqual(CellState.Alive);
+      expect(cell.CurrentState).not.toEqual(CellState.Dead);
+  });
+
+  it("Given 1 Alive Cell With 1Alive Neighbour When Mutate Then Cell Dies", function() {
+      var cell = new GameOfLife.Cell(1, 1, CellState.Alive);
+
+      cell.AddNeighbours([new GameOfLife.Cell(1, 2, CellState.Alive)]);
+      cell.Mutate();
+
+      console.log(cell.NextState);
+      expect(cell.NextState).toEqual(CellState.Dead);
   });
 });
